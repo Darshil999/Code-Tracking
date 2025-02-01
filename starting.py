9d5f25fcb432c2959e93f92e67113c378bd2c182
Dhruv18052003-web <kalyanidarshil@gmail.com>
2025-02-01 23:51:47 +0530

    Starting Basics

---
+++ starting.py
@@ -0,0 +1,20 @@
+import datetime
+import csv
+
+current_date = datetime.datetime.now().strftime("%d-%m-%Y")
+print(current_date)
+
+no_of_lines = int(input("Enter the number of lines you did today(Approximate) : "))
+print(no_of_lines)
+
+with open("Code_Tracker.csv","a", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow([current_date,no_of_lines])
+
+print("CSV file written successfully")
+
+
+with open("Code_Tracker.csv","r") as file:
+    reader = csv.reader(file)
+    for i in reader:
+        print(i)
